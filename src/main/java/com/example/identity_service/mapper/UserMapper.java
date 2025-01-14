8d05cfd737f134737624e6490ccc76d81175f226
tiamo405 <nam05052002@gmail.com>
2025-01-15 00:23:18 +0700

    init

--- src/main/java/com/example/identity_service/mapper/UserMapper.java
+++ src/main/java/com/example/identity_service/mapper/UserMapper.java
@@ -10,6 +10,7 @@ import org.mapstruct.MappingTarget;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User toUser(UserCreationRequest request);
+
     UserResponse toUserResponse(User user);
     void updateUser(@MappingTarget User user, UserUpdateRequest request);
 }
