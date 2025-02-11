ba09aeb557f3843b34de517dce3c53657b9b0c9c
tiamo405 <nam05052002@gmail.com>
2025-02-11 10:15:00 +0700

    feat: validation nang cao

--- src/main/java/com/example/identity_service/dto/request/UserCreationRequest.java
+++ src/main/java/com/example/identity_service/dto/request/UserCreationRequest.java
@@ -21,7 +21,7 @@ public class UserCreationRequest {
     String firstName;
     String lastName;
 
-    @DobConstraint(min = 18, message = "INVALID_DOB")
+    @DobConstraint(min = 16, message = "INVALID_DOB")
     LocalDate dob;
 
 
--- src/main/java/com/example/identity_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/identity_service/exception/GlobalExceptionHandler.java
@@ -1,14 +1,22 @@
 package com.example.identity_service.exception;
 
 import com.example.identity_service.dto.request.ApiResponse;
+import jakarta.validation.ConstraintViolation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
+import java.util.Objects;
+
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final String MIN_ATTIBUTE = "min";
 
     @ExceptionHandler(value = RuntimeException.class)
     ResponseEntity<ApiResponse> handlingRuntimeException(RuntimeException e) {
@@ -58,15 +66,32 @@ public class GlobalExceptionHandler {
     ResponseEntity<ApiResponse> handlingValidation(MethodArgumentNotValidException e) {
         String enumKey = e.getFieldError().getDefaultMessage();
         ErrorCode errorCode = ErrorCode.INVALID_KEY;
+        Map<String, Object> attributes = null ;
+
         try {
             errorCode = ErrorCode.valueOf(enumKey);
+
+            var constraintViolation = e.getBindingResult()
+                    .getAllErrors().get(0).unwrap(ConstraintViolation.class);
+
+            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
+            log.info("Attributes: {}", attributes.toString());
+
         } catch (IllegalArgumentException ex) {
 
         }
 
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(errorCode.getMessage());
+        apiResponse.setMessage(Objects.nonNull(attributes) ?
+                mapAttributes(errorCode.getMessage(), attributes)
+                : errorCode.getMessage());
         return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    private String mapAttributes(String message, Map<String, Object> attributes) {
+        String minValue = String.valueOf(attributes.get(MIN_ATTIBUTE));
+
+        return message.replace("{" + MIN_ATTIBUTE + "}", minValue);
     }
 }
