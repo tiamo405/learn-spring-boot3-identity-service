ef84e26d4f866e71a4b60b4532d55a78d8fcc523
tiamo405 <nam05052002@gmail.com>
2025-02-16 17:52:11 +0700

    fix: auto create user admin first run have role admin

--- src/main/java/com/example/identity_service/configuration/ApplicationInitConfig.java
+++ src/main/java/com/example/identity_service/configuration/ApplicationInitConfig.java
@@ -1,12 +1,12 @@
 package com.example.identity_service.configuration;
 
+import com.example.identity_service.entity.Role;
 import com.example.identity_service.entity.User;
-import com.example.identity_service.enums.Role;
+import com.example.identity_service.repository.RoleRepository;
 import com.example.identity_service.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,18 +21,26 @@ import java.util.HashSet;
 public class ApplicationInitConfig {
 
     private PasswordEncoder passwordEncoder;
-
+    RoleRepository roleRepository;
     @Bean
     ApplicationRunner applicationRunner(UserRepository userRepository) {
 
         return args -> {
             if(userRepository.findByUsername("admin").isEmpty()){
-//                var roles = new HashSet<String>();
-//                roles.add(Role.ADMIN.name());
+
+                Role adminRole = roleRepository.save(Role.builder()
+                            .name("ADMIN")
+                            .description("admin role")
+//                            .permissions(new HashSet<>(permissions))
+                            .build());
+
+                var roles = new HashSet<Role>();
+                roles.add(adminRole);
+
                 User user = User.builder()
                         .username("admin")
                         .password(passwordEncoder.encode("admin"))
-//                        .roles(roles)
+                        .roles(roles)
                         .build();
                 userRepository.save(user);
                 log.warn("default admin user created password is admin, please change it");
